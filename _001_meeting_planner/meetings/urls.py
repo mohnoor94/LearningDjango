53f904c276d52610f5abb2e8646fd57acf0bf038
mohnoor94 <moh.noor94@gmail.com>
2020-07-18 22:45:10 +0300

    Add and Validate Forms using ModelForms

--- _001_meeting_planner/meetings/urls.py
+++ _001_meeting_planner/meetings/urls.py
@@ -5,4 +5,5 @@ from meetings import views
 urlpatterns = [
     path('<int:id>', views.detail, name='detail'),
     path('rooms', views.rooms, name='rooms'),
+    path('create', views.create, name='create'),
 ]
--- _001_meeting_planner/meetings/views.py
+++ _001_meeting_planner/meetings/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.forms import modelform_factory
+from django.shortcuts import render, get_object_or_404, redirect
 
 from meetings.models import Meeting, Room
 
@@ -14,4 +15,25 @@ def detail(request, id):
 def rooms(request):
     return render(request, 'meetings/rooms.html', {
         'rooms': Room.objects.all(),
+    })
+
+
+MeetingForm = modelform_factory(Meeting, exclude=[])  # Generate a MeetingForm class
+
+
+def create(request):
+    if request.method == 'POST':
+        # form has been submitted, process data
+        form = MeetingForm(request.POST)  # create a new MeetingForm from the data that the user submitted!
+        if form.is_valid():
+            form.save()  # save to the DB
+            return redirect('home')  # redirects to 'welcome' page url
+
+    else:
+        # request = 'GET'
+        # Show the form so the user can fill and submit it!
+        form = MeetingForm()
+
+    return render(request, 'meetings/create.html', {
+        'form': form
     })
